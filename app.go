5732741f3ef3b41f7d52d96733c5f4b51b917b30
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-02-26 19:48:23 +0200

    fix(failed-to-open-db-connection): fix app breaking

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -23,10 +24,27 @@ func (app *App) InitialiseDB(DBUser string, DBPass string, DBName string) error 
 	}
 
 	app.Router = mux.NewRouter().StrictSlash(true)
+	app.handleRoutes()
 
 	return nil
 }
 
 func (app *App) Run(addr string) {
+	fmt.Printf("Server running on %v", addr)
 	log.Fatal(http.ListenAndServe(addr, app.Router))
+}
+
+// private to app.go file
+func (app *App) getProducts(w http.ResponseWriter, r *http.Request) {
+
+	var response []byte
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(200)
+	w.Write(response)
+
+}
+
+func (app *App) handleRoutes() {
+	app.Router.HandleFunc("/products", app.getProducts).Methods("GET")
 }
