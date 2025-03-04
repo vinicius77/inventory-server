00f641e01a7c2774b6c7d7d88b551ff158806e12
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-03-04 19:56:51 +0200

    feat(models): create Product struct and getProducts method to query against the DB

---
+++ model.go
@@ -0,0 +1,34 @@
+package main
+
+import "database/sql"
+
+type Product struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Quantity int     `json:"quantity"`
+	Price    float64 `json:"price"`
+}
+
+func getProducts(db *sql.DB) ([]Product, error) {
+	query := "SELECT id, name, quantity, price FROM products"
+	rows, err := db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	products := []Product{}
+
+	for rows.Next() {
+		var product Product
+
+		err := rows.Scan(&product.ID, &product.Name, &product.Quantity, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, product)
+	}
+
+	return products, nil
+}
